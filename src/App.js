ec72af149fde36857568e0afa83ff5839d202dd9
Vineet Parekh <pvineet44@gmail.com>
2021-05-28 13:18:00 +0530

    Initialization

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      React app        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCswD5NC7krBRu6Ws4YiH8Ln_5tcIaua2M",
+    authDomain: "vp-emazon-clone.firebaseapp.com",
+    projectId: "vp-emazon-clone",
+    storageBucket: "vp-emazon-clone.appspot.com",
+    messagingSenderId: "279946226368",
+    appId: "1:279946226368:web:0f676b983d7ad758d9db25",
+    measurementId: "G-G5Q0PLRFED"
+  };
