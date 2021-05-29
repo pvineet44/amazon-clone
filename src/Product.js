77acb5e129db5bffb58c41a10536ad82238b0dec
Vineet Parekh <pvineet44@gmail.com>
2021-05-29 14:19:41 +0530

    Product

---
+++ src/Product.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Product.css'
+
+function Product() {
+    return (
+        <div className='product'>
+            <div className='product__info'>
+                <p>The Lean Startup</p>
+                <p className='product__price'>
+                    <small>$</small>
+                    <strong>19.99</strong>
+                </p>
+                <div className='product__rating'>
+                    <p>⭐️</p>
+                </div>
+            </div>
+            <img src='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg'/>
+            <button>Add to basket</button>
+        </div>
+    )
+}
+
+export default Product
