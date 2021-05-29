7c5753c8dc42b60846a55bf9d4b03296fe293278
Vineet Parekh <pvineet44@gmail.com>
2021-05-29 15:19:52 +0530

    products

--- src/Home.js
+++ src/Home.js
@@ -12,17 +12,54 @@ function Home() {
           src='https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg'
           alt='home-image'
         />
-      <div className='home__row'>
-        <Product />
-        <Product />
-        {/* Product */}
-      </div>
-      <div className='home__row'>
-        {/* Product */}
-        {/* Product */}
-        {/* Product */}
-      </div>
-      <div className='home__row'>{/* Product */}</div>
+        <div className='home__row'>
+          <Product
+            id={12345678}
+            title='The lean startup'
+            price={29.99}
+            image='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg'
+            rating={5}
+          />
+          <Product
+          id={12345678}
+          title='The lean startup'
+          price={29.99}
+          image='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg'
+          rating={5}
+          />
+        </div>
+        <div className='home__row'>
+          <Product 
+          id='4903050'
+          title='Samsung LC49RG90SSUXEN 49 Inch Curved LED Gaming Monitor'
+          price={199.99}
+          image='https://images-na.ssl-images-amazon.com/images/I/71Swqqe7XAL._AC_SX466_.jpg'
+          rating={5}
+          />
+          <Product
+          id='23445930'
+          title='Amazon Echo (3rd generation) | Smart speaker with Alexa, Charcoal Fabric'
+          price={98.99}
+          image='https://media.very.co.uk/i/very/P6LTG_SQ1_0000000071_CHARCOAL_SLf?$300x400_retinamobilex2$'
+          rating={5} 
+          />
+          <Product 
+          id='3254354345'
+          title='New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)'
+          price={598.99}
+          image='https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcRJ7Y3xOkenybRs-EGxDpBwp5HB-nyCEHr5SiFqlrqYAzGvPsaVgoU&usqp=CAc'
+          rating={4}
+          />
+        </div>
+        <div className='home__row'>
+        <Product 
+          id='4903050'
+          title='Samsung LC49RG90SSUXEN 49 Inch Curved LED Gaming Monitor'
+          price={199.99}
+          image='https://images-na.ssl-images-amazon.com/images/I/6125mFrzr6L._AC_SX355_.jpg'
+          rating={5}
+          />
+        </div>
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -1,23 +1,27 @@
-import React from 'react'
-import './Product.css'
+import React from 'react';
+import './Product.css';
 
-function Product() {
-    return (
-        <div className='product'>
-            <div className='product__info'>
-                <p>The Lean Startup</p>
-                <p className='product__price'>
-                    <small>$</small>
-                    <strong>19.99</strong>
-                </p>
-                <div className='product__rating'>
-                    <p>⭐️</p>
-                </div>
-            </div>
-            <img src='https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg'/>
-            <button>Add to basket</button>
+function Product({ id, title, image, price, rating }) {
+  return (
+    <div className='product'>
+      <div className='product__info'>
+        <p>{title}</p>
+        <p className='product__price'>
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className='product__rating'>
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐️</p>
+            ))}
         </div>
-    )
+      </div>
+      <img src={image} />
+      <button>Add to basket</button>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
