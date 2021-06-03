f4dfa8981b7bf0b77ff9afe522c3a0d8ea97b89a
Vineet Parekh <pvineet44@gmail.com>
2021-06-03 18:31:31 +0530

    Updated baseURL to cloud function url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/vp-emazon-clone/us-central1/api', //THE API(cloud function) URL
+    // baseURL: 'http://localhost:5001/vp-emazon-clone/us-central1/api', //LOCAL
+    baseURL: 'https://us-central1-vp-emazon-clone.cloudfunctions.net/api'
 });
 
 export default instance;
